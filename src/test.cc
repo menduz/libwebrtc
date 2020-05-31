4ce111a3f36edcf61ca5e6068c4c7bbbb0d1e8f5
Agustin Mendez <agustin.mz.92@gmail.com>
2020-05-31 17:00:51 -0300

    test new build

--- src/test.cc
+++ src/test.cc
@@ -1,21 +1,25 @@
+// #include <iostream>
+// #include <string>
+// #include <thread>
+
+// // #include <rtc_base/physical_socket_server.h>
+// // #include <rtc_base/ssl_adapter.h>
+// // #include <rtc_base/thread.h>
+// #include <system_wrappers/include/field_trial.h>
+
+// int main(int argc, char *argv[])
+// {
+//   webrtc::field_trial::InitFieldTrialsFromString("");
+
+//   std::cout << std::this_thread::get_id() << ":"
+//             << "Main thread" << std::endl;
+
+//   return 0;
+// }
+
 #include <iostream>
-#include <memory>
-#include <mutex>
-#include <queue>
-#include <string>
-#include <thread>
-
-#include <rtc_base/physical_socket_server.h>
-#include <rtc_base/ssl_adapter.h>
-#include <rtc_base/thread.h>
-#include <system_wrappers/include/field_trial.h>
-
-int main(int argc, char *argv[])
+int main()
 {
-  webrtc::field_trial::InitFieldTrialsFromString("");
-
-  std::cout << std::this_thread::get_id() << ":"
-            << "Main thread" << std::endl;
-
+  std::cout << "Hello, World!" << std::endl;
   return 0;
 }
