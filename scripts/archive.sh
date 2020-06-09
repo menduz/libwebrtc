93bb58026b7a1e93202f8d931cececaa8e0d2920
Agustin Mendez <agustin.mz.92@gmail.com>
2020-06-08 22:29:57 -0300

    fix include headers

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -15,7 +15,7 @@ cp "$BASE_DIR/build/$RELEASE_TYPE/obj/libwebrtc.lib" "$ARTIFACTS_DIR/lib" || tru
 cp "$BASE_DIR/build/$RELEASE_TYPE/obj/pc/libpeerconnection.lib" "$ARTIFACTS_DIR/lib" || true
 cp "$BASE_DIR/build/$RELEASE_TYPE/obj/api/libjingle_peerconnection_api.lib" "$ARTIFACTS_DIR/lib" || true
 
-find "$BASE_DIR/build/external/libwebrtc/download/src" -name "*.h" -print | cpio -pd "$ARTIFACTS_DIR/include"
+find "$BASE_DIR/download/src" -name "*.h" -print | cpio -pd "$ARTIFACTS_DIR/include"
 
 # create tar
 tar -cvf artifact.tar "$ARTIFACTS_DIR"
